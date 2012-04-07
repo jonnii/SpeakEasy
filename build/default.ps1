40374b3752b9483022b95a9082d4bff60a557cfa
jonnii <me@jonnii.com>
2012-04-07 12:54:38 -0400

    start working on packaging

--- build/default.ps1
+++ build/default.ps1
@@ -1,18 +1,31 @@
 task default -depends Package
 
 task Package -depends Test {
+	$packagesDirectory = '..\targets\packages'
+	
+	if (Test-Path -path $packagesDirectory)
+	{
+		rm $packagesDirectory -recurse -force
+	}
 
+	mkdir $packagesDirectory
+	cp ..\build\package.nuspec ..\targets\packages
+
+	mkdir ..\targets\packages\lib\net40
+	cp ..\targets\resticle\resticle.* ..\targets\packages\lib\net40
+
+	..\src\.nuget\nuget.exe pack -outputdirectory .\targets\packages ..\targets\packages\package.nuspec 
 }
 
 task Test -depends Compile, Clean { 
-  
+  # run specs
+  # integration tets
 }
 
 task Compile -depends Clean { 
-  
+  # call msbuild
 }
 
 task Clean { 
-  
+  # clear out ..\targets
 }
-
