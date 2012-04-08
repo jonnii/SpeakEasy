b9e4bfb30a612ed3c7001f737c97948082aab80c
jonnii <me@jonnii.com>
2012-04-08 15:57:10 -0400

    add common assembly info

--- src/Resticle/Properties/AssemblyInfo.cs
+++ src/Resticle/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Resticle.IntegrationTests")]
+[assembly: AssemblyTitle("Resticle")]
 [assembly: Guid("aa50801e-7c6a-4810-829f-6d7405643751")]
