187573805e16ea57b1828e4aec954821e2801c41
jonnii <me@jonnii.com>
2012-04-10 23:12:52 -0400

    more build related stuff

--- build/default.ps1
+++ build/default.ps1
@@ -1,3 +1,7 @@
+properties {
+	$buildName = "dev"
+}
+
 task default -depends Package
 
 task Package -depends Test {
@@ -23,15 +27,39 @@ task Publish -depends Package {
 	..\src\.nuget\nuget.exe Push $package.fullname
 }
 
-task Test -depends Compile, Clean { 
-  # run specs
-  # integration tets
+task CopyTools {
+	
 }
 
-task Compile -depends Clean { 
+task Test -depends Clean,CopyTools,Compile { 
+  # run specs
+  ..\src\packages\Machine.Specifications.0.5.2.0\tools\mspec-clr4.exe .\..\targets\specifications\speakeasy.specifications.dll
+
+  # run integration tests somehow (need a local nunit-console.exe, or switch to xunit?)
+}
+
+task UpdateAssemblyInfo {
+	$version = get-content ..\VERSION
+
+	get-content ..\src\CommonAssemblyInfo.cs | 
+        %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $version } |
+        %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $version }  > ..\src\CommonAssemblyInfo.cs.tmp
+	
+	mv ..\src\CommonAssemblyInfo.cs.tmp ..\src\CommonAssemblyInfo.cs -force
+}
+
+task Compile -depends Clean,UpdateAssemblyInfo { 
   # call msbuild
+  $options = "/p:Configuration=Debug"
+  $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+  
+  Push-Location ..\src
+  $build = $msbuild + ' "SpeakEasy.sln" ' + $options + " /t:Build"
+
+  iex $build
+  Pop-Location
 }
 
 task Clean { 
-  # clear out ..\targets
+  rm -recurse -force ..\targets
 }
