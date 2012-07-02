36f5efded812109f6f26963b4703ae8411db16f7
jonnii <me@jonnii.com>
2012-07-02 14:01:46 -0400

    add ability to authenticate with a specific username/password in windows authenticator

--- src/SpeakEasy/Authenticators/WindowsAuthenticator.cs
+++ src/SpeakEasy/Authenticators/WindowsAuthenticator.cs
@@ -2,11 +2,47 @@ using System.Net;
 
 namespace SpeakEasy.Authenticators
 {
+    /// <summary>
+    /// The windows authenticator will authenticate
+    /// an http request with windows credentials
+    /// </summary>
     public class WindowsAuthenticator : IAuthenticator
     {
+        private readonly ICredentials credentials;
+
+        /// <summary>
+        /// Creates a windows authenticator with the default credentials
+        /// </summary>
+        public WindowsAuthenticator()
+            : this(CredentialCache.DefaultCredentials)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a windows authenticator with a specific username 
+        /// and password
+        /// </summary>
+        /// <param name="username">The username to authenticate with</param>
+        /// <param name="password">The password to authenticate with</param>
+        public WindowsAuthenticator(string username, string password)
+            : this(new NetworkCredential(username, password))
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a windows authenticator with a specific credentials
+        /// </summary>
+        /// <param name="credentials">The credentials to authenticate with</param>
+        public WindowsAuthenticator(ICredentials credentials)
+        {
+            this.credentials = credentials;
+        }
+
         public void Authenticate(IHttpRequest httpRequest)
         {
-            httpRequest.Credentials = CredentialCache.DefaultCredentials;
+            httpRequest.Credentials = credentials;
         }
     }
 }
