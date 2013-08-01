21c01cea3a9158097c986edd6bc2f5460f00e490
jonnii <me@jonnii.com>
2013-08-01 16:18:18 -0400

    add simple json serializer.

---
+++ src/SpeakEasy/Serializers/SimpleJsonSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeakEasy.Serializers
+{
+    public class SimpleJsonSerializer : StringBasedSerializer
+    {
+        public override IEnumerable<string> SupportedMediaTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    "application/json",
+                    "text/json",
+                    "text/x-json",
+                    "text/javascript"
+                };
+            }
+        }
+
+        public override string Serialize<T>(T t)
+        {
+            return SimpleJson.SerializeObject(t);
+        }
+
+        public override T DeserializeString<T>(string body, DeserializationSettings deserializationSettings)
+        {
+            if (deserializationSettings.SkipRootElement)
+            {
+                throw new NotSupportedException("Cannot skip root element with SimpleJsonSerializer");
+            }
+
+            if (deserializationSettings.HasRootElementPath)
+            {
+                throw new NotSupportedException("Cannot navigate root element path with SimpleJsonSerializer");
+            }
+
+            return SimpleJson.DeserializeObject<T>(body);
+        }
+    }
+}
