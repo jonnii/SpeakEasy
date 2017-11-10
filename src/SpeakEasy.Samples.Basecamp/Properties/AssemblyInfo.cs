edfc5e49cc409e6ff531d8ff6af3e167db172572
jonnii <me@jonnii.com>
2017-11-10 12:50:45 -0500

    nuke examples

--- src/SpeakEasy.Samples.Basecamp/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SpeakEasy.Samples.Basecamp")]
-[assembly: Guid("4b5b16c0-a861-49b2-b099-6800fc8d88ce")]
--- src/SpeakEasy.Samples.RabbitMq/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("860df643-04a8-42ef-99e8-521e772b26fa")]
