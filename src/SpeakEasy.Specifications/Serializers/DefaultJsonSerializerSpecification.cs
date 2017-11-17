ee1453e4068d8543b8c310f8b48a0f8bac8f8236
jonnii <me@jonnii.com>
2017-11-17 10:54:08 -0500

    clean up

--- src/SpeakEasy.Specifications/Serializers/DefaultJsonSerializerSpecification.cs
+++ src/SpeakEasy.Specifications/Serializers/DefaultJsonSerializerSpecification.cs
@@ -1,63 +1,67 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using Machine.Fakes;
 using Machine.Specifications;
 using SpeakEasy.Serializers;
 
 namespace SpeakEasy.Specifications.Serializers
 {
     [Subject(typeof(DefaultJsonSerializer))]
-    public class DefaultJsonSerializerSpecification : WithSubject<DefaultJsonSerializer>
+    public class DefaultJsonSerializerSpecification
     {
         static MemoryStream stream;
 
+        static DefaultJsonSerializer serializer;
+
         Establish context = () =>
+        {
             stream = new MemoryStream();
+            serializer = new DefaultJsonSerializer();
+        };
 
         public class when_deserializing_dynamic
         {
+            static dynamic deserialized;
+
             Because of = () =>
-                deserialized = Subject.Deserialize<dynamic>(new MemoryStream(Encoding.UTF8.GetBytes(@"{ ""message"": ""hi sir"" }")));
+                deserialized = serializer.Deserialize<dynamic>(new MemoryStream(Encoding.UTF8.GetBytes(@"{ ""message"": ""hi sir"" }")));
 
             It should_deserialize_items_when_array = () =>
                 ((string)deserialized.message).ShouldEqual("hi sir");
-
-            static dynamic deserialized;
         }
 
         class when_deserializing_array_with_default_settings
         {
+            static List<string> deserialized;
+
             Establish context = () =>
             {
-                Subject.SerializeAsync(stream, new[] { "a", "b", "c" }).Await();
+                serializer.SerializeAsync(stream, new[] { "a", "b", "c" }).Await();
                 stream.Position = 0;
             };
 
             Because of = () =>
-                deserialized = Subject.Deserialize<List<string>>(stream);
+                deserialized = serializer.Deserialize<List<string>>(stream);
 
             It should_deserialize_items_when_array = () =>
                 deserialized.Count.ShouldEqual(3);
-
-            static List<string> deserialized;
         }
 
         class when_deserializing_object_with_default_settings
         {
+            static Person deserialized;
+
             Establish context = () =>
             {
-                Subject.SerializeAsync(stream, new Person { Name = "fred", Age = 30 }).Await();
+                serializer.SerializeAsync(stream, new Person { Name = "fred", Age = 30 }).Await();
                 stream.Position = 0;
             };
 
             Because of = () =>
-                deserialized = Subject.Deserialize<Person>(stream);
+                deserialized = serializer.Deserialize<Person>(stream);
 
             It should_deserialize_items_when_array = () =>
                 deserialized.Name.ShouldEqual("fred");
-
-            static Person deserialized;
         }
 
         public class Person
@@ -65,11 +69,6 @@ namespace SpeakEasy.Specifications.Serializers
             public string Name { get; set; }
 
             public int Age { get; set; }
-        }
-
-        public class Container<T>
-        {
-            public T Root { get; set; }
         }
     }
 }
