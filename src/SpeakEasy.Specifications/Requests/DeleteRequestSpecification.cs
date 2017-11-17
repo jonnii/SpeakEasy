87c207f69ba2b5f6fb14e1d8bfdeff1b5aa0595b
jonnii <me@jonnii.com>
2017-11-17 13:28:02 -0500

    oops

--- src/SpeakEasy.Specifications/Requests/DeleteRequestSpecification.cs
+++ src/SpeakEasy.Specifications/Requests/DeleteRequestSpecification.cs
@@ -11,6 +11,9 @@ namespace SpeakEasy.Specifications.Requests
 
         class in_general
         {
+            Establish context = () =>
+                request = new DeleteRequest(new Resource("hello"));
+
             It should_have_delete_method = () =>
                 request.HttpMethod.ShouldEqual(HttpMethod.Delete);
         }
