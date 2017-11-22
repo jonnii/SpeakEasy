055c9755122aae91e0752e383e7b91b66e5368f0
jonnii <me@jonnii.com>
2017-11-22 10:32:40 -0500

    better tests

--- src/SpeakEasy.IntegrationTests/MiddlewareTests.cs
+++ src/SpeakEasy.IntegrationTests/MiddlewareTests.cs
@@ -9,47 +9,60 @@ namespace SpeakEasy.IntegrationTests
 {
     public class MiddlewareTests
     {
-        [Fact]
-        public async void ShouldAddCustomHeaders()
+        [Theory]
+        [InlineData("bob")]
+        [InlineData("jeff")]
+        public async void ShouldAddCustomHeaders(string name)
         {
             var settings = new HttpClientSettings();
 
             settings.AddMiddleware(new ConsoleLoggingMiddleware());
-            settings.AddMiddleware(new CustomHeadersMiddleware());
+            settings.AddMiddleware(new CustomHeadersMiddleware(name));
 
             var client = HttpClient.Create("http://localhost:1337/api", settings);
 
             var response = await client
-                .Get("products/1");
+                .Get("middleware/echo-header", new { headerName = "x-special-name" })
+                .OnOk()
+                .AsString();
 
-            Assert.Contains(":1337/api/products/1", response.State.RequestUrl.ToString());
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains(name, response);
         }
 
-        [Fact]
-        public async void ShouldHaveCustomUserAgent()
+        [Theory]
+        [InlineData("FancyAgent")]
+        [InlineData("MozillaForReal")]
+        public async void ShouldHaveCustomUserAgent(string userAgent)
         {
             var settings = new HttpClientSettings();
 
             settings.AddMiddleware(new ConsoleLoggingMiddleware());
-            settings.ReplaceMiddleware(new UserAgentMiddleware(new UserAgent("MyFancyUserAgent")));
+            settings.ReplaceMiddleware(new UserAgentMiddleware(new UserAgent(userAgent)));
 
             var client = HttpClient.Create("http://localhost:1337/api", settings);
 
             var response = await client
-                .Get("products/1");
+                .Get("middleware/echo-user-agent")
+                .OnOk()
+                .AsString();
 
-            Assert.Contains(":1337/api/products/1", response.State.RequestUrl.ToString());
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(userAgent, response);
         }
 
         public class CustomHeadersMiddleware : IHttpMiddleware
         {
+            private readonly string name;
+
+            public CustomHeadersMiddleware(string name)
+            {
+                this.name = name;
+            }
+
             public IHttpMiddleware Next { get; set; }
 
             public async Task<IHttpResponse> Invoke(IHttpRequest request, CancellationToken cancellationToken)
             {
-                request.AddHeader("x-special-header", "frank");
+                request.AddHeader("x-special-name", name);
                 request.AddHeader(x => x.ExpectContinue = true);
 
                 return await Next.Invoke(request, cancellationToken).ConfigureAwait(false);
